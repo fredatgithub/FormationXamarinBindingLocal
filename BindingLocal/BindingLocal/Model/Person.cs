ad115d0b2fad2e259b1a2d011651fdbbda33d056
Fred <domaine75@gmail.com>
2017-09-06 16:18:42 +0100

    Add on Property change event

--- BindingLocal/BindingLocal/Model/Person.cs
+++ BindingLocal/BindingLocal/Model/Person.cs
@@ -1,8 +1,33 @@
-﻿namespace BindingLocal.Model
+﻿using System.ComponentModel;
+using BindingLocal.Annotations;
+
+namespace BindingLocal.Model
 {
-  internal class Person
+  internal class Person : INotifyPropertyChanged
   {
+    private int _age;
     public string Name { get; set; }
-    public int Age { get; set; }
+
+    public int Age
+    {
+      get { return _age; }
+      set
+      {
+        if (this._age == value)
+        {
+          return;
+        }
+
+        _age = value;
+        OnPropertyChanged(nameof(Age));
+      }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName = null)
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
   }
 }

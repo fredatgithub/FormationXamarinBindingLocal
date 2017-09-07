ab2581aeaed3761a28df6c6181459f10ab2d3597
Fred <domaine75@gmail.com>
2017-09-07 08:55:57 +0100

    Add method to color textbox

---
+++ BindingLocal/BindingLocal/Model/IntToColor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using XF =  Xamarin.Forms;
+
+namespace BindingLocal.Model
+{
+  internal class IntToColor : XF.IValueConverter
+  {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      if (targetType != typeof(XF.Color))
+      {
+        return null;
+      }
+
+      int age = int.Parse(value.ToString());
+      return age > 30 ? XF.Color.Green : XF.Color.Aqua;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
